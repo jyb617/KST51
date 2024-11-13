f2be3892c3bf4db89602c0d4dc399f5ba7d56f2f
jyb617 <121718993+jyb617@users.noreply.github.com>
2024-11-13 17:52:06 +0800

    Add files via upload

---
+++ 14密码锁/I2C.c
@@ -0,0 +1,109 @@
+#include<reg52.h>
+#include <intrins.h>
+#define I2CDelay(){_nop_();_nop_();_nop_();_nop_();}
+sbit I2C_SCL=P3^7;
+sbit I2C_SDA=P3^6;
+
+void I2CStart()
+{
+	I2C_SCL=1;
+	I2C_SDA=1;
+	I2CDelay();
+	I2C_SDA=0;
+	I2CDelay();
+	I2C_SCL=0;
+}
+
+void I2CStop()
+{
+	I2C_SCL=0;
+	I2C_SDA=0;
+	I2CDelay();
+	I2C_SCL=1;;
+	I2CDelay();
+	I2C_SDA=1;
+	I2CDelay();
+}
+
+bit I2CWrite(unsigned char dat)
+{
+	bit ack;
+	unsigned char mate;
+	for(mate=0x80;mate!=0;mate>>1)
+	{
+		if((dat&mate)!=0)
+			I2C_SDA=1;
+		else
+		{
+			I2C_SDA=0;
+		}
+		I2CDelay();
+		I2C_SCL=1;
+		I2CDelay();
+		I2C_SCL=0;
+	}
+	I2C_SDA=1;
+	I2CDelay();
+	I2C_SCL=1;
+	ack=I2C_SDA;
+	I2CDelay();
+	I2C_SCL=0;
+	return (~ack);
+}
+
+unsigned char I2CReadNAK()
+{
+	unsigned char dat;
+	unsigned char mate;
+	I2C_SDA=1;
+	for(mate=0x80;mate!=0;mate>>1)
+	{
+		I2CDelay();
+		I2C_SCL=1;
+		if(I2C_SDA==0)
+		{
+			dat&=~mate;
+		}
+		else
+		{
+			dat|=mate;
+		}
+		I2CDelay();
+		I2C_SCL=0;
+	}
+	I2C_SDA=1;
+	I2CDelay();
+	I2C_SCL=1;
+	I2CDelay();
+	I2C_SCL=0;
+	
+	return dat;
+}
+unsigned char I2CReadACK()
+{
+	unsigned char dat;
+	unsigned char mate;
+	I2C_SDA=1;
+	for(mate=0x80;mate!=0;mate>>1)
+	{
+		I2CDelay();
+		I2C_SCL=1;
+		if(I2C_SDA==0)
+		{
+			dat&=~mate;
+		}
+		else
+		{
+			dat|=mate;
+		}
+		I2CDelay();
+		I2C_SCL=0;
+	}
+	I2C_SDA=0;
+	I2CDelay();
+	I2C_SCL=1;
+	I2CDelay();
+	I2C_SCL=0;
+	
+	return dat;
+}
